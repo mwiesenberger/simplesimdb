58052db647fe1d0b842f44617c2bd648886bcc27
Matthias <mattwi@fysik.dtu.dk>
2022-06-16 17:56:23 +0200

    Bump version number in setup to 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="simplesimdb",
-    version="1.0.1",
+    version="1.0.2",
     author="Matthias Wiesenberger",
     author_email="mattwi@fysik.dtu.dk",
     description="Create, access and manage simple simulation data",
